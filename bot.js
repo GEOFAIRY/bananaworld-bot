86be63f8b41c99325d84659484abed7223589dda
Unknown <kst84@uclive.ac.nz>
2018-10-11 18:41:37 +1300

    Added bot code base!

---
+++ bot.js
@@ -0,0 +1,31 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+function intervalFunc() {
+	const channel = client.channels.find('id', '403093923418865664');
+	channel.send("@here Beep");
+	var date = new Date();
+	var current_hour = date.getHours();
+	console.log('Beeped at ' + current_hour);
+}
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === '!ping') {
+    msg.reply('pong!');
+  }
+  if (msg.content === '!quit') {
+    msg.reply('later fags')
+    process.exit();
+  }
+  //msg.react("🇳");
+  //msg.react("🇴");
+  //msg.react("🇺");
+});
+
+client.login('NDk5NDc4NDY4NjQ4NzYzMzkz.Dp-TsQ.gZTSs1-giBJNevQplfUbCk4InZI');
+
+setInterval(intervalFunc, 3600000);
